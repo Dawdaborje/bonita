4a6344f4333e8370f19aaff483a95ad3e3872499
Dawda Borje Kujabi <dawdaborjekujabi@gmail.com>
2025-07-25 08:47:02 +0000

    Adding shadchn components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import '../app.css';
-	
+	import { ModeWatcher } from 'mode-watcher';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppBar from '$lib/components/layout/appBar.svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+<Sidebar.Provider>
+	<AppBar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children()}
+	</main>
+</Sidebar.Provider>
